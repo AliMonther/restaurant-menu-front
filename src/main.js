0263e21055dda9e1beb1fb9ed4ed57db1461e66f
Ali Monzer <ali.m@storexweb.com>
2023-02-22 11:22:38 +0200

    prepare project structure

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import VueSweetalert2 from 'vue-sweetalert2';
+import router from "./routers/main";
+import { createPinia } from "pinia";
+import 'sweetalert2/dist/sweetalert2.min.css';
+import "primeflex/primeflex.css";
+import "primevue/resources/primevue.min.css";
+import "primevue/resources/themes/md-light-indigo/theme.css";
+import "primeicons/primeicons.css";
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+    .use(createPinia())
+    .use(router)
+    .use(VueSweetalert2);
+
+app.mount("#app");
